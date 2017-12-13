47ca06249b13fbbb9774986b62daaeea0a534a8c
Alex Roodo <alexroodo6@gmail.com>
2017-12-13 22:44:26 +0300

    Added Main class and release all functional (look: https://habrahabr.ru/post/44031/)

---
+++ src/main/java/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        SimpleGrepProgram simpleGrepProgram = new SimpleGrepProgram(args);
+
+        simpleGrepProgram.grep();
+    }
+}
